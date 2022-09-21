10729b7d59d4ecfcbdd35b66e182925ba1a178d2
David-Main <work.davidmainoo@gmail.com>
2022-09-21 02:19:39 +0000

    Add hero image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import dishes from "./data/dishes.json";
 
 function App() {
+  const [currentIndex, setCurrentIndex]: [number, Function] = useState(0);
   return (
-    <div className="App container mx-auto px-10 bg-background bg-center bg-cover min-h-screen min-w-screen">
-      <div className="flex justify-between items-center">
+    <div className="App container  mx-auto px-10 bg-background bg-center  bg-cover min-h-screen  min-w-screen">
+      <section className="hero mx-auto w-max relative  max-w-lg -mt-10">
+        <img src={dishes[currentIndex].imageLarge} alt={dishes[currentIndex].name} />
+      </section>
+      <div className="flex -mt-72 pb-4 justify-around max-w-max gap-20 mx-auto items-center">
         {dishes.map((item) => {
           return <Card {...{...item}} />;
         })}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,20 +13,20 @@ const Card = ({...dish}: DishType) => {
   return (
     <>
       <figure className="relative h-max max-w-max">
-        <img src={image} alt={name} className=" max-w-[14rem] max-h-[14rem] relative top-[8rem] z-10 mx-auto pr-5" />
-        <figcaption className="px-4 rounded-3xl shadow-2xl shadow-neutral-400 pt-16 bg-white max-w-max relative">
-          <div className="card pb-6 pt-16 text-center">
-            <p className="text-2xl font-semibold">{name}</p>
+        <img src={image} alt={name} className=" max-w-[10em] relative top-[5rem] z-10 mx-auto" />
+        <figcaption className="px-4 rounded-3xl shadow-2xl shadow-neutral-400 pt-12 bg-white max-w-max relative">
+          <div className="card pb-6 pt-10 text-center">
+            <p className="text-xl font-semibold">{name}</p>
             <span className="text-gray-400 text-lg font-semibold">{calories}</span>
           </div>
-          <div className="py-5 pb-7 border-t-2 flex justify-between items-center gap-28">
-            <div className="flex flex-col gap-5">
+          <div className="py-2 pb-7 border-t-2 flex justify-between items-center gap-16">
+            <div className="flex flex-col gap-2">
               <span className="text-gray-400 font-semibold">Time</span>
-              <span className="font-bold">{time}</span>
+              <span className="font-bold text-sm">{time}</span>
             </div>
-            <div className="flex flex-col gap-5 items-end">
+            <div className="flex flex-col gap-2 items-end">
               <span className="text-gray-400 font-semibold">Person</span>
-              <span className="font-bold">{person}</span>
+              <span className="font-bold text-sm">{person}</span>
             </div>
           </div>
         </figcaption>
